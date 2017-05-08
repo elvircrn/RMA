a0e9641829753f8ffc38ca2df4d8ecc871ab0afb
elvircn <elvircrn@gmail.com>
2017-05-08 23:28:31 +0200

    onAttach bug fix

--- LV1/app/src/main/java/ba/unsa/etf/rma/elvircrn/rma17_17455/MusiciansListFragment.java
+++ LV1/app/src/main/java/ba/unsa/etf/rma/elvircrn/rma17_17455/MusiciansListFragment.java
@@ -137,12 +137,6 @@ public class MusiciansListFragment extends Fragment implements SearchArtist.IOnM
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
